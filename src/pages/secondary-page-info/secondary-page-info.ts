706ce4fb6368ae54bd638f94778d00be63c12bf1
chikavi1 <chikavi@hotmail.com>
2019-06-17 23:57:56 -0500

    al chile no recuerdo ajaja pero fue algo importante

--- src/pages/secondary-page-info/secondary-page-info.ts
+++ src/pages/secondary-page-info/secondary-page-info.ts
@@ -1,14 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the SecondaryPageInfoPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
+import { NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-secondary-page-info',
   templateUrl: 'secondary-page-info.html',
