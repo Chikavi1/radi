dde4c3e944ce8822656cc52b00082b57b7f190b5
chikavi1 <chikavi@hotmail.com>
2019-06-17 23:59:37 -0500

    agregue la pagina aliados

---
+++ src/pages/aliados/aliados.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AliadosPage } from './aliados';
+
+@NgModule({
+  declarations: [
+    AliadosPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(AliadosPage),
+  ],
+})
+export class AliadosPageModule {}
---
+++ src/pages/aliados/aliados.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { ApiProvider } from '../../providers/api/api';
+import { ShowAlliesPage } from '../show-allies/show-allies';
+@Component({
+  selector: 'page-aliados',
+  templateUrl: 'aliados.html',
+})
+export class AliadosPage {
+  allies : any[];
+  constructor(public navCtrl: NavController, public navParams: NavParams,public AP:ApiProvider) {
+  	this.AP.getAllies().subscribe(
+  			(data)  => {this.allies = data,console.log(data)},
+  			(error) => {console.log(error)}
+  				);
+  	}
+  
+doRefresh(refresher) {
+    setTimeout(() => {
+       this.AP.getAllies().subscribe(
+       (data) => {this.allies = data},
+       (error) =>{console.log(error)}
+       );
+            refresher.complete();
+    }, 2000);
+  }
+goDetails(data){
+  this.navCtrl.push(ShowAlliesPage,{datos : data});
+}
+}
