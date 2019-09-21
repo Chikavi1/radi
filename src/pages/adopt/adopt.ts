5ede3e29c1927efbf2402febfa65769a1fdbc591
chikavi1 <chikavi@hotmail.com>
2019-09-21 10:33:43 -0500

    modifique la vistas del adopt y agregue la ubicacion

--- src/pages/adopt/adopt.ts
+++ src/pages/adopt/adopt.ts
@@ -1,21 +1,21 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {  NavController, NavParams } from 'ionic-angular';
+import { ApiProvider } from '../../providers/api/api';
+import { FormAdoptPage } from '../form-adopt/form-adopt';
+import { DogProfilePage } from '../dog-profile/dog-profile';
 
-/**
- * Generated class for the AdoptPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
-@IonicPage()
 @Component({
   selector: 'page-adopt',
   templateUrl: 'adopt.html',
 })
 export class AdoptPage {
+  dogs;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public api:ApiProvider) {
+    this.api.getStatus("no adoptado").subscribe(
+           (data) => { this.dogs = data;},
+           (error) =>{ });
   }
 
   ionViewDidLoad() {
@@ -25,10 +25,29 @@ export class AdoptPage {
   doRefresh(refresher) {
  
     setTimeout(() => {
-      console.log('Async operation has ended');
       refresher.complete();
     }, 750);
   }
-
-
+perfil_perro(codigo){
+  console.log(codigo);
+      this.api.searchQrCode(codigo).subscribe(
+       (data) => {
+         this.goToProfileDog(data);
+       },
+       (error) =>{}
+       );
+   }
+  iraformulario(id){
+    console.log(id);
+    this.navCtrl.push(FormAdoptPage,{idperro : id});
+  }
+  goToProfileDog(dog : any){ 
+  this.navCtrl.push(DogProfilePage,{dog : dog});
 }
+  
+  }
+
+
+
+
+
