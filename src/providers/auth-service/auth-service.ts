f5a5cdd99d6481c555400fe7aa42f69c081a0b6d
chikavi1 <chikavi@hotmail.com>
2019-10-10 19:13:17 -0500

    modifique la api

--- src/providers/auth-service/auth-service.ts
+++ src/providers/auth-service/auth-service.ts
@@ -12,13 +12,10 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class AuthServiceProvider {
   
-  // Change to this http://ed43bb3b.ngrok.io/api/login
-  static readonly LOGIN_URL = 'http://contoh.dev/api/login';
-  // Change to this http://ed43bb3b.ngrok.io/api/register
-  static readonly REGISTER_URL = 'http://contoh.dev/api/register';
+
   access: boolean;
   token: string;
-  baseUrl = "https://www.chikavi.com/api/";
+  baseUrl:string = "https://www.chikavi.com/api/";
   baseUrlLocal:string = "http://127.0.0.1:8000/api/";
 
   constructor(public http: Http) {}
@@ -26,16 +23,17 @@ export class AuthServiceProvider {
   // Login
   loginlaravel(email,password):any{
     let headers = new Headers();
-      headers.append('Content-Type','application/x-www-form-urlencoded');
-      headers.append('X-Requested-With', 'XMLHttpRequest');
+     // headers.append('Content-Type','application/x-www-form-urlencoded');
+     // headers.append('X-Requested-With', 'XMLHttpRequest');
+
      return this.http.get(this.baseUrl+"ingresar_cuenta?"+"email="+email+"&password="+password,{headers: headers});
   }
   getUser(id):any{
 
      let headers = new Headers();
-      headers.append('Content-Type','application/x-www-form-urlencoded');
-      headers.append('Authorization', 'Bearer '+ localStorage.getItem("token"));
-    return this.http.get(this.baseUrl+"user?user="+id,{headers: headers});
+      //headers.append('Content-Type','application/x-www-form-urlencoded');
+      //headers.append('Authorization', 'Bearer '+ localStorage.getItem("token"));
+    return this.http.get(this.baseUrl+"profile?id="+id,{headers: headers});
   }
 
   create_user(name,email,psw,c_pws){
